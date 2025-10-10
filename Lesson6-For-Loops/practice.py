e1f534bc98d4a93bd054004009ce4df6e27f7123
adrdom26-max <adrdom26-max>
2025-10-10 10:27:39 -0400

    practice almost complete

--- Lesson6-For-Loops/practice.py
+++ Lesson6-For-Loops/practice.py
@@ -1,9 +1,43 @@
-number = 5
-for i in range(1,11):
-    result = number * i
-    print(f"5 * {i} = {result}")
+# number = 5
+# for i in range(1,11):
+#     result = number * i
+#     print(f"5 * {i} = {result}")
 
-word = "Python"
-reversed_word = ""
-for char in word:
-    reversed_word = char + reversed_word 
+# word = "Python"
+# reversed_word = ""
+# for char in word:
+#     reversed_word = char + reversed_word 
+
+# for i in range(10, 0, -1):
+#     print(i)
+# print("Blast Off")
+
+# text= "Hello World"
+# count = 0
+# for char in text:
+#     if char in "oO":
+#         count += 1
+# print("Count")
+
+# password = "Pass123"
+# upper = False
+# lower = False
+# digit = False
+# for char in password:
+#     if 'A' <= char <= 'Z':
+#         upper = True
+#     elif 'a' <= char <= 'z':
+#         lower = True
+#     elif '0' <= char <= '9':
+#         digit = True
+# valid = len(password) >= 8 and upper and lower and digit
+# print(valid)
+
+# total = 0
+# for i in range(1,101):
+#     total += i
+# print(total)
+
+text = "abc123xyz456"
+numbers = ''
+for char in text:
